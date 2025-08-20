e7b51bc6f537cb71768e95a04b92872b09e36385
7sumona02 <sumona.23548@sscbs.du.ac.in>
2025-08-20 17:18:23 +0530

    added

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,87 @@
-import Image from "next/image";
+import { Doto, Figtree } from 'next/font/google'
+import Link from 'next/link'
+ 
+const doto = Doto({
+  weight: '400',
+  subsets: ['latin'],
+})
 
-export default function Home() {
+const fig = Figtree({
+  weight: '400',
+  subsets: ['latin'],
+})
+
+const workData = [
+  {
+    name: 'Mental Health AI Website',
+    image: 'https://cdn.cosmos.so/d1827ff1-de6f-4ea9-83b4-1c311aa799c3?format=jpeg',
+    link: '/',
+  },
+  {
+    name: 'Restaurant Website',
+    image: 'https://cdn.cosmos.so/da0d64b0-99e4-4fc3-994c-af2a656a1035?format=jpeg',
+    link: '/',
+  },
+  {
+    name: 'Portfolio Website',
+    image: 'https://cdn.cosmos.so/0cde1dd9-4d17-4343-bb28-44dd6677ce1c?format=jpeg',
+    link: '/',
+  },
+  {
+    name: 'Creative Landing Page',
+    image: 'https://cdn.cosmos.so/664146b7-05fd-4847-b886-9ef6c1fc9f2e?format=jpeg',
+    link: '/',
+  },
+]
+
+const Page = () => {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="h-screen w-full bg-black text-white p-10 flex items-start justify-between gap-20 overflow-hidden">
+      {/* Left content */}
+      <div>
+        <Link href='/' className={`${doto.className} text-2xl leading-6`}>
+          SUMONA <br /> BISWAS
+        </Link>
+        <div className="pt-20 space-y-4">
+          <div className={`${fig.className} text-lg text-white/60 font-medium`}>
+            Info
+          </div>
+          <div className={`${fig.className} text-lg text-white max-w-lg leading-tighter`}>
+           I&apos;m a Frontend developer and Framer designer passionate about creating clean, modern, and user-focused websites. I help businesses and individuals bring their vision to life with minimal yet impactful designs. Check out my <Link href='https://sumona.me' className='hover:text-white/60 duration-500 transition-all space-x-1'><span className='underline-style'>developer portfolio</span><span className='text-sm'>↗</span></Link>{" "}
+to see how I turn ideas into engaging digital experiences.
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+         <div className="pt-20 space-y-4">
+          <div className={`${fig.className} text-lg text-white/60 font-medium`}>
+            Contact
+          </div>
+          <div className={`${fig.className} text-lg text-white max-w-lg leading-tighter flex flex-col gap-1`}>
+           <Link href='mailto:sumonadotwork@gmail.com' className='hover:text-white/60 duration-500 transition-all space-x-1'><span className='underline-style'>sumonadotwork@gmail.com</span><span className='text-sm'>↗</span></Link>
+           <Link href='https://sumona.me' className='hover:text-white/60 duration-500 transition-all space-x-1'><span className='underline-style'>sumona.me</span><span className='text-sm'>↗</span></Link>
+           <Link href='https://x.com/weebdev_san' className='hover:text-white/60 duration-500 transition-all space-x-1'><span className='underline-style'>twitter</span><span className='text-sm'>↗</span></Link>
+          </div>
+        </div>
+      </div>
+
+      {/* Right content (scrollable works) */}
+      <div className="h-[94vh] overflow-y-scroll hide-scrollbar flex flex-col gap-10">
+        {workData.map((work, i) => (
+          <Link key={i} href={work.link}>
+            <div className="w-[45vw] cursor-pointer relative group">
+              <img
+                src={work.image}
+                alt={work.name}
+                className="w-full h-auto object-contain hover:opacity-50 duration-500 transition-all relative"
+              />
+              <div className="absolute left-4 group-hover:bottom-4 bottom-0 opacity-0 group-hover:opacity-100 duration-500 transition-all">
+                <div className="font-medium text-lg">{work.name}</div>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
-  );
+  )
 }
+
+export default Page
